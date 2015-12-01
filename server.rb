91c3a649b24b80dd55bfa66ea0e50feb4f3e730d
i-tsvetkov <i.tsvetkov.4@gmail.com>
2015-12-01 18:56:05 +0200

    sane variable names

--- server.rb
+++ server.rb
@@ -3,27 +3,27 @@ require 'open-uri'
 require 'json'
 require './alicebot.rb'
 
-def tr(from, to, text)
+def translate(from, to, text)
   text = URI.escape(text)
   url = "https://translate.google.com/m?hl=bg&sl=#{from}&tl=#{to}&ie=UTF-8&prev=_m&q=#{text}"
   ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/45.0.2454.101 Chrome/45.0.2454.101 Safari/537.36'
   open(url, {'user-agent' => ua}).read.match(/<div dir="ltr" class="t0">([^<]+)<\/div>/)[1]
 end
 
-def wa(q)
-  q = URI.escape(q)
+def wolfram_alpha(query)
+  q = URI.escape(query)
   url = "http://m.wolframalpha.com/input/?i=#{q}&x=0&y=0"
   open(url).read.match(/^\s*Result.*$/).to_s[/alt="([^"]+)"/, 1]
 end
 
-def ddg(q)
-  q = URI.escape(q)
+def duckduckgo(query)
+  q = URI.escape(query)
   url = "http://api.duckduckgo.com/?q=#{q}&format=json"
   JSON.parse(open(url).read)['Answer'][/>([^>]+)<\/a>$/, 1]
 end
 
-def cb(q)
-  `python cleverbot.py '#{q}'`
+def cleverbot(query)
+  `python cleverbot.py '#{query}'`
 end
 
 def speak(msg)
@@ -38,7 +38,7 @@ ab = AliceBot.new
 server.mount_proc '/hal' do |req, res|
   q = req.query['q']
   q = tr(:bg, :en, q)
-  ans = wa(q) || ddg(q) || ab.talk(q) || cb(q) || "no data"
+  ans = wolfram_alpha(q) || duckduckgo(q) || ab.talk(q) || cleverbot(q) || "no data"
   ans = tr(:en, :bg, ans)
   res.body = ans
   speak(ans)
