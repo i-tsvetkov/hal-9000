062f6245b0e4fb8a1eaefcf0943a9c00c166e64e
i-tsvetkov <i.tsvetkov.4@gmail.com>
2016-07-10 11:29:04 +0300

    better voice recognition cycle, page style improvements also code refactoring

--- hal.js
+++ hal.js
@@ -1,17 +1,28 @@
 window.onload = function () {
   var recognition = new webkitSpeechRecognition();
-  recognition.continuous = true;
+  recognition.continuous = false;
   recognition.interimResults = false;
   recognition.lang = 'bg-BG';
+
   var res = document.getElementById('res');
   var ans = document.getElementById('ans');
+  var img = document.getElementById('img');
+  var sep = document.getElementById('sep');
   var audio = document.getElementById('audio');
+
+  audio.onended = () => recognition.start();
+  img.onclick   = () => recognition.start();
+
+  recognition.onstart = () => img.className = 'active';
+  recognition.onend   = () => img.className = '';
+
   recognition.onresult = function(event) {
     res.textContent =
       event.results[event.results.length-1][0].transcript;
     var xhr = new XMLHttpRequest();
     xhr.onload = function (e) {
       ans.textContent = e.target.responseText;
+      sep.style.visibility = 'visible';
       audio.src = 'espeak.ogg';
       audio.load();
       audio.play();
@@ -21,6 +32,7 @@ window.onload = function () {
       + encodeURIComponent(res.textContent));
     xhr.send();
   };
+
   recognition.start();
 };
 
